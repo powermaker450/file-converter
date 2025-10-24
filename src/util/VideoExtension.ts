9b7b84bd6242a29532176995ebc576799b2c20b1
povario <contact@povario.com>
2025-10-23 22:22:05 -0400

    Remove webm conversion until ffmpeg wasm fixes issues

--- src/util/VideoExtension.ts
+++ src/util/VideoExtension.ts
@@ -1,10 +1,10 @@
-type Key = "MP4" | "MKV" | "WEBM";
-type Value = ".mp4" | ".mkv" | ".webm";
+type Key = "MP4" | "MKV" /* | "WEBM" */;
+type Value = ".mp4" | ".mkv" /* | ".webm" */;
 
 export const VideoExtension: Record<Key, Value> = {
   MP4: ".mp4",
   MKV: ".mkv",
-  WEBM: ".webm",
+  // WEBM: ".webm",
 };
 
 export type VideoExtension = Value;
